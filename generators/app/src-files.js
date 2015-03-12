5be2ef8acec87a7fd03532c63dc351971b81d8e6
pcruz <pedro.cruz@log.pt>
2015-03-12 16:55:57 +0000

    Move marionette files to folder

--- generators/app/src-files.js
+++ generators/app/src-files.js
@@ -5,27 +5,27 @@ module.exports = {
         var ext = generator.config.get('engine') === 'dust' ? '.dust' : 'hbs';
         return [
             {
-                src: 'main.js',
+                src: 'marionette/main.js',
                 dst: 'src/scripts/main.js'
             },
             {
-                src: 'app.js',
+                src: 'marionette/app.js',
                 dst: 'src/scripts/app/app.js'
             },
             {
-                src: 'radio.js',
+                src: 'marionette/radio.js',
                 dst: 'src/scripts/app/radio.js'
             },
             {
-                src: 'behaviors.js',
+                src: 'marionette/behaviors.js',
                 dst: 'src/scripts/app/behaviors/behaviors.js'
             },
             {
-                src: 'root-view.js',
+                src: 'marionette/root-view.js',
                 dst: 'src/scripts/app/views/root-view.js'
             },
             {
-                src: 'root.html',
+                src: 'marionette/root.html',
                 dst: 'src/scripts/app/templates/root.' + ext
             }
         ];
