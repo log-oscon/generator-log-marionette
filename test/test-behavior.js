97ed7498614f22320fc0d38a91e92e1830226702
pcruz <pedro.cruz@log.pt>
2015-03-18 10:57:45 +0000

    Add behaviour generator spec

---
+++ test/test-behavior.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var utils  = require('../utils'),
+    assert = require('yeoman-generator').assert;
+
+describe('log-marionette:behavior', function () {
+    before(function (done) {
+        utils.runSubGenerator('../generators/behavior', {
+            args: ['log'],
+            done: done
+        });
+    });
+
+    it('should create src and spec', function () {
+        assert.file([
+            'src/scripts/app/behaviors/log-behavior.js'
+        ]);
+    });
+
+    it('should set the src and spec contents', function () {
+        assert.fileContent('src/scripts/app/behaviors/log-behavior.js', /var LogBehavior = Marionette.Behavior.extend/);
+        assert.fileContent('src/scripts/app/behaviors/log-behavior.js', /module.exports = LogBehavior;/);
+    });
+});
